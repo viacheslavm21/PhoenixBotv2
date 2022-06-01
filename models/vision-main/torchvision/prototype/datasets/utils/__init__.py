af0c4788a282352004c5e4a37321b74e0841a0a9
vikt <vikt-atl30@ya.ru>
2022-06-01 09:46:30 +0300

    delete unnecessary stuff

--- models/vision-main/torchvision/prototype/datasets/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import _internal  # usort: skip
-from ._dataset import DatasetConfig, DatasetInfo, Dataset
-from ._query import SampleQuery
-from ._resource import OnlineResource, HttpResource, GDriveResource, ManualDownloadResource, KaggleDownloadResource
